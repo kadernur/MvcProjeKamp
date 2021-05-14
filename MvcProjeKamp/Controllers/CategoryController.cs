56e805959eb21500eee1bb55612b4b439c03177e
Kader <Lenovo@ACER-COMPUTER>
2021-05-14 17:54:43 +0300

    1.görev

---
+++ MvcProjeKamp/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
